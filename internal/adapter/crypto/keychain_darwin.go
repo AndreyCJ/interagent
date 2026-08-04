99aa6091dbfd4851b0c151376bb06d6923eb3d15
Andrey Chebotar <andrey.chebotar.me@gmail.com>
2026-08-04 12:33:07 +0300

    fix(crypto): only rotate keychain key on errSecItemNotFound

--- internal/adapter/crypto/keychain_darwin.go
+++ internal/adapter/crypto/keychain_darwin.go
@@ -5,6 +5,7 @@ package crypto
 import (
 	"crypto/rand"
 	"encoding/hex"
+	"errors"
 	"os/exec"
 	"strings"
 )
@@ -14,6 +15,8 @@ const (
 	keychainAccount = "master"
 )
 
+var securityCmd = exec.Command
+
 type Keychain struct{}
 
 func NewKeychain() *Keychain { return &Keychain{} }
@@ -22,6 +25,9 @@ func (k *Keychain) GetOrCreate() ([]byte, error) {
 	key, err := k.find()
 	if err == nil {
 		return key, nil
+	}
+	if !isItemNotFound(err) {
+		return nil, err
 	}
 	key = make([]byte, 32)
 	if _, err := rand.Read(key); err != nil {
@@ -34,7 +40,7 @@ func (k *Keychain) GetOrCreate() ([]byte, error) {
 }
 
 func (k *Keychain) find() ([]byte, error) {
-	out, err := exec.Command("security", "find-generic-password", "-s", keychainService, "-a", keychainAccount, "-w").Output()
+	out, err := securityCmd("security", "find-generic-password", "-s", keychainService, "-a", keychainAccount, "-w").Output()
 	if err != nil {
 		return nil, err
 	}
@@ -42,5 +48,10 @@ func (k *Keychain) find() ([]byte, error) {
 }
 
 func (k *Keychain) add(key []byte) error {
-	return exec.Command("security", "add-generic-password", "-U", "-s", keychainService, "-a", keychainAccount, "-w", hex.EncodeToString(key)).Run()
+	return securityCmd("security", "add-generic-password", "-U", "-s", keychainService, "-a", keychainAccount, "-w", hex.EncodeToString(key)).Run()
+}
+
+func isItemNotFound(err error) bool {
+	var exitErr *exec.ExitError
+	return errors.As(err, &exitErr) && exitErr.ExitCode() == 44
 }
